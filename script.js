fa902c919feac24d006550227dee3a0c23cd6919
verter187 <verter187@inbox.ru>
2022-11-21 20:28:54 +0300

    new commit 20221121_3

--- script.js
+++ script.js
@@ -55,3 +55,87 @@ function writeYourGenres() {
   }
 }
 writeYourGenres();
+
+function calculateVolumeAndArea(base) {
+  return typeof base === "number" && base % 1 === 0 && base >= 0
+    ? `Объем куба: ${Math.pow(base, 3)}, площадь всей поверхности: ${
+        Math.pow(base, 2) * 6
+      }`
+    : "При вычислении произошла ошибка";
+}
+
+console.log(calculateVolumeAndArea(15));
+
+// Место для второй задачи
+function getCoupeNumber(cNum) {
+  if (typeof cNum !== "number" || cNum % 1 !== 0 || cNum < 0) {
+    return "Ошибка. Проверьте правильность введенного номера места";
+  } else if (cNum === 0 || cNum > 36) {
+    return "Таких мест в вагоне не существует";
+  } else {
+    return Math.ceil(cNum / 4);
+  }
+}
+
+console.log(getCoupeNumber(39));
+
+function getTimeFromMinutes(minutes) {
+  if (typeof minutes !== "number" || minutes % 1 !== 0 || minutes < 0) {
+    return "Ошибка, проверьте данные";
+  } else {
+    let hours = Math.floor(minutes / 60),
+      mins = minutes - hours * 60,
+      nameMins = "минут",
+      nameHours = "часов";
+    if (String(mins).slice(-1) === "1") {
+      nameMins = "минута";
+    } else if (["2", "3", "4"].includes(String(mins).slice(-1))) {
+      nameMins = "минуты";
+    }
+
+    if (String(hours).slice(-1) === "1") {
+      nameHours = "час";
+    } else if (["2", "3", "4"].includes(String(hours).slice(-1))) {
+      nameHours = "часа";
+    }
+    return `Это ${hours} ${nameHours} и ${mins} ${nameMins}`;
+  }
+}
+
+console.log(getTimeFromMinutes(-150));
+
+function findMaxNumber(...args) {
+  if (
+    args.length !== 4 ||
+    args.filter((arg) => typeof arg !== "number").length > 0
+  ) {
+    return 0;
+  } else {
+    return Math.max(...args);
+  }
+}
+
+console.log(findMaxNumber(1, 5, "6", "10"));
+
+function fib(counter) {
+  if (typeof counter !== "number" || counter % 1 !== 0 || counter < 0) {
+    return "";
+  }
+
+  let str = " ";
+  let arr = [];
+  for (let i = 0; i < counter; i++) {
+    if (i === 0) {
+      arr[i] = 0;
+      str = str + " 0";
+    } else if (i === 1) {
+      arr[i] = 1;
+      str = str + " 1";
+    } else {
+      arr[i] = arr[i - 2] + arr[i - 1];
+      str = str + ` ${arr[i]}`;
+    }
+  }
+  return str.trim();
+}
+fib(0);
